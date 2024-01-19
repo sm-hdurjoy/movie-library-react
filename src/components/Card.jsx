2f1a7381b0eeaed806eab7759a2fe1b25edda1f9
sm-hdurjoy <smhazzazdurjoy@gmail.com>
2024-01-19 11:38:20 +0600

    useFetch custom hook added and dynamic API done

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,43 +1,27 @@
 import { Link } from "react-router-dom";
-import Logo from "../assets/logo.png";
+import NoImageAvailable from "../assets/images/notAvailable.jpg";
 
-export const Card = () => {
+export const Card = ({ movie }) => {
+  const { id, original_title, overview, poster_path, release_date } = movie;
+  const moviePosterImage = poster_path ? `https://image.tmdb.org/t/p/original/${poster_path}`: NoImageAvailable
+
   return (
     <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 m-3">
-      <Link to="#">
-        <img className="rounded-t-lg" src={Logo} alt="" />
+      <Link to={`/movie/${id}`}>
+        <img className="rounded-t-lg" src={moviePosterImage} alt="" />
       </Link>
       <div className="p-5">
-        <Link to="#">
+        <Link to={`/movie/${id}`}>
           <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            Noteworthy technology acquisitions 2021
+            {original_title}
           </h5>
         </Link>
         <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-          Here are the biggest enterprise technology acquisitions of 2021 so
-          far, in reverse chronological order.
+          {overview}
         </p>
-        <Link
-          to="#"
-          className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-        >
-          Read more
-          <svg
-            className="w-3.5 h-3.5 ml-2"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 14 10"
-          >
-            <path
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M1 5h12m0 0L9 1m4 4L9 9"
-            />
-          </svg>
-        </Link>
+        <p className="mb-3 font-normal text-gray-700 dark:text-gray-100">
+          Release Date : {release_date}
+        </p>
       </div>
     </div>
   );
--- src/pages/MovieList.jsx
+++ src/pages/MovieList.jsx
@@ -1,12 +1,16 @@
 import { Card } from "../components";
+import { useFetch } from "../hooks/useFetch";
 
-export const MovieList = () => {
+export const MovieList = (apiPath) => {
+  const { data: movies } = useFetch(apiPath);
+
   return (
     <main>
       <section className="max-w-7xl mx-auto py-7">
-        <div className="flex justify-start flex-wrap ">
-          <Card />
-          <Card />
+        <div className="flex justify-center flex-wrap ">
+          {movies && movies.map((movie) => (
+            <Card key={movie.id} movie={movie} />
+          ))}
         </div>
       </section>
     </main>
